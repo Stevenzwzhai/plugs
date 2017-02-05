e9cc568f81a63736dbacf8abad988f150615b39b
Stevenzwzhai <zzwdzsw@sina.com>
2017-02-05 17:12:03 +0800

    js解析url

--- parseQueryString/index.js
+++ parseQueryString/index.js
@@ -1,22 +1,26 @@
 /**
  * Created by Stevenzwzhai on 2017/2/5.
  */
-var url = window.location;
-var href = url.protocol;
-var protocol = url.protocol;
-var host = url.host;
-var port = url.port;
-var pathname = url.pathname;
-var origin = url.origin;
-var hostname = url.hostname;
-var search  = url.search;
-
-var query = {};
-var searchArr = search.slice(1, search.length).split('&');
-for(var i = 0;i<searchArr.length;i++){
-    var tempArr = searchArr[i].split('=');
-    if(typeof query[tempArr[0]] == "undefined"){
-        query[tempArr[0]] = tempArr[1];
+(function(){
+    var url = window.location;
+    var search  = url.search;
+    var query = {};
+    var searchArr = search.slice(1, search.length).split('&');
+    for(var i = 0;i<searchArr.length;i++){
+        var tempArr = searchArr[i].split('=');
+        if(typeof query[tempArr[0]] == "undefined"){
+            query[tempArr[0]] = tempArr[1];
+        }
     }
-}
-console.log(query);
+    return {
+
+        href : url.protocol,
+        protocol : url.protocol,
+        host : url.host,
+        port : url.port,
+        pathname : url.pathname,
+        origin : url.origin,
+        hostname : url.hostname,
+        query : query
+    }
+})()
