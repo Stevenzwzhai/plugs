68cfc93c5bfab391ba663fd7afe4e4d9dcda675d
Stevenzwzhai <zzwdzsw@sina.cn>
2017-09-17 22:43:51 +0800

    deal number

---
+++ Number/index.js
@@ -0,0 +1,37 @@
+/**
+    常见的数字处理
+*/
+
+//取整
+function getAbs(num){
+    if(typeof num !== "number"){
+        throw new Error("请传入数字");
+    }
+
+    // return num.toFixed(0);
+    return num > 0 ? Math.floor(num) : Math.ceil(num);
+}
+//保留小数
+function getFixed(num, deci){
+    if(typeof num !== "number"){
+        throw new Error("请传入数字");
+    }
+
+    return num.toFixed(deci||2);
+}
+
+//取千分位
+function getThousands(num){
+    if(typeof num !== "number"){
+        throw new Error("请传入数字");
+    }
+
+    var numberA= String(num).split('.');
+    var numberB = (numberA[0] || 0).toString(), result = '';
+    while (numberB.length > 3) {
+        result = ',' + numberB.slice(-3) + result;
+        numberB = numberB.slice(0, numberB.length - 3);
+    }
+    if (numberB) { result = numberB + result; }
+    return result+(numberA[1]?('.'+numberA[1]):'');
+}
