b05384a9ec62609c5c0b67b332e9bea36a6d6d4a
Stevenzhai <zzwdzsw@sina.cn>
2017-09-21 01:55:15 -0500

    Update classification.js

--- 无限级分类/classification.js
+++ 无限级分类/classification.js
@@ -34,3 +34,26 @@
             }
         }
     }
+
+//更加简便的方式
+function listToTree(list) {
+  const map = {};
+  let node;
+  let roots = [];
+  let i;
+  for (i = 0; i < list.length; i += 1) {
+    map[list[i].id] = i; // initialize the map
+    list[i].children = []; // initialize the children
+  }
+  for (i = 0; i < list.length; i += 1) {
+    node = list[i];
+    if (node.parentId != '-1') {
+      // if you have dangling branches check that map[node.parentId] exists
+      node.parentName = list[map[node.parentId]].label;
+      list[map[node.parentId]].children.push(node);
+    } else {
+      roots.push(node);
+    }
+  }
+  return roots;
+}
