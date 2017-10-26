07a067695eb18c07cd5fb902f7772e018a7b1eab
Stevenzwzhai <zzwdzsw@sina.cn>
2017-10-26 20:46:10 +0800

    add deepClone

---
+++ deepClone/index.js
@@ -0,0 +1,43 @@
+function deepClone(data) {
+    let type = this.getType(data);
+    let obj;
+    if (type === 'array') {
+        obj = [];
+    } else if (type === 'object') {
+        obj = {};
+    } else {
+        //不再具有下一层次
+        return data;
+    }
+    if (type === 'array') {
+        for (let i = 0, len = data.length; i < len; i++) {
+            obj.push(this.deepClone(data[i]));
+        }
+    } else if (type === 'object') {
+        for (let key in data) {
+            obj[key] = this.deepClone(data[key]);
+        }
+    }
+    return obj;
+},
+function getType(obj) {
+    let toString = Object.prototype.toString;
+    let map = {
+        '[object Boolean]': 'boolean',
+        '[object Number]': 'number',
+        '[object String]': 'string',
+        '[object Function]': 'function',
+        '[object Array]': 'array',
+        '[object Date]': 'date',
+        '[object RegExp]': 'regExp',
+        '[object Undefined]': 'undefined',
+        '[object Null]': 'null',
+        '[object Object]': 'object'
+    };
+    if (obj instanceof Element) {
+        return 'element';
+    }
+    return map[toString.call(obj)];
+}
+
+export default deepClone;
